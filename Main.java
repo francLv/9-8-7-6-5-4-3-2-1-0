c04748067cdbac23c59fe704701c7bf07f2ed0ee
Vladimir Levin <94986766+francLv@users.noreply.github.com>
2022-01-25 12:39:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int i = 9;
+        while (i >= 0) {
+          System.out.print(i + " ");
+          i--;
+        }
+        
+    }
+}
